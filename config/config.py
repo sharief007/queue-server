ac511fc3030e3b6b0f52305d50fec8b8af011b9a
Mohammad Sharief Baig <shariefmohammad007@gmail.com>
2025-08-18 17:51:52 +0530

    Clean up old architecture files.

--- config/config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-data_path = './data/'
-queue_prefix = 'queue-'
-
-queue_filename = 'queue.dat'
-meta_filename = 'meta.dat'
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/sharief007/queue-server
-
-go 1.21.1
